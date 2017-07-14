942e960ae86eccdaae55deb2e986960c16c38705
laharun <laharun92@gmail.com>
2017-07-14 12:46:17 +0700

    e

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ezodiak</title>
+</head>
+<body>
+
+
+<script src='https://api.instagram.com/v1/users/self/?access_token=dd4b01d1f70445ec9d89d3b77194c833
+' type="text/javascript"></script>
+</body>
+</html>
+
 <?php
-https://api.instagram.com/v1/users/self/?access_token=dd4b01d1f70445ec9d89d3b77194c833
 ?>
